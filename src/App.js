62842c6462d0d83b8f5b0c888d9d126dd2bcdb53
Robert Wilson <youmufan@gmail.com>
2018-03-02 18:36:10 -0500

    created redux store, count reducer, and implemented increment/decrement actions. however, the react app is not re-rendering

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import './App.css';
+import { createStore } from 'redux';
+
+//initial state
+const initialState = 0;
+//reducer
+const countReducer = (state = initialState, action) =>
+{
+  switch(action.type){
+
+    case 'INCREMENT':
+      state = state + 1;
+      return state;
+
+    case 'DECREMENT':
+      state = state - 1;
+      return state;
+    
+    default:
+      return state;
+  }
+};
+
+//creation of the store
+const store = createStore(countReducer,
+window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      Hello
+      <div>
+      <h1>{store.getState()}</h1>
+      <button onClick={store.dispatch({type: "INCREMENT" })}>+</button>
+      <button onClick={store.dispatch({type: "DECREMENT" })}>-</button>
       </div>
     );
   }
 }
 
 export default App;
+ 
